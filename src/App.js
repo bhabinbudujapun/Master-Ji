654467ac251d18fd0bc4ef437204f6741272baac
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-26 12:12:08 +0545

    initial commit and challange submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import OtpForm from "./OtpForm/OtpForm";
+import CourseList from "./CourseList/CourseList";
+import Batches from "./Batches/Batches";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/otp-form" element={<OtpForm />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/batches" element={<Batches />} />
+        <Route path="/" element={<Navigate to="/otp-form" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Batches/Batches.jsx
@@ -0,0 +1,9 @@
+function Batches() {
+  return (
+    <h1 className="flex items-center justify-center text-5xl font-bold text-white mt-8">
+      Batches
+    </h1>
+  );
+}
+
+export default Batches;
