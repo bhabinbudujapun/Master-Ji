24796410e11b1387dab1221596cc2e69fe49683e
Bhabin <poon.bhabin6332@gmail.com>
2025-03-19 13:46:02 +0545

    initial commit of restruct the collection of project

---
+++ masterjireact/src/CourseList/CourseList.jsx
@@ -0,0 +1,9 @@
+function CourseList() {
+  return (
+    <h1 className="flex items-center justify-center text-5xl font-bold text-white mt-8">
+      Course List
+    </h1>
+  );
+}
+
+export default CourseList;
