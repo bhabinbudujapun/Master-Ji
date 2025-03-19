297666e409e784b83e1a1eba8825916c383284a5
Bhabin <poon.bhabin6332@gmail.com>
2025-03-19 13:46:45 +0545

    restruct files

--- src/Batches/Batches.jsx
+++
@@ -1,9 +0,0 @@
-function Batches() {
-  return (
-    <h1 className="flex items-center justify-center text-5xl font-bold text-white mt-8">
-      Batches
-    </h1>
-  );
-}
-
-export default Batches;
--- src/CourseList/CourseList.jsx
+++
@@ -1,9 +0,0 @@
-function CourseList() {
-  return (
-    <h1 className="flex items-center justify-center text-5xl font-bold text-white mt-8">
-      Course List
-    </h1>
-  );
-}
-
-export default CourseList;
