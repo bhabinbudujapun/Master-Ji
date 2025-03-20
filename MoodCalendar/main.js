f6e5f1347eecb1b502475dc036fc5f6cdd1695d8
Bhabin <poon.bhabin6332@gmail.com>
2025-03-20 16:41:12 +0545

    toogle functionality for selector options

--- MoodCalendar/main.js
+++ MoodCalendar/main.js
@@ -1,3 +1,7 @@
+const days = document.querySelector(".days");
+const viewButton = document.querySelectorAll(".view-selector");
+const views = document.querySelectorAll(".day-view, .week-view, .month-view");
+
 let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
@@ -24,7 +28,51 @@ function dateOfDays(year, month, day) {
   return daysOfWeek[dayOfWeek];
 }
 
-let dateOfDay = dateOfDays(year, month, currentDate);
-console.log(dateOfDay);
-let numberOfDays = getDaysInMonth(year, month + 1);
-console.log(year, month + 1, currentDate, numberOfDays, day + 1);
+function displayCalendar() {
+  //Month and Year - title
+  const monthName = date.toLocaleString("en-US", { month: "long" });
+  const monthYearTitle = document.querySelector(".month-title");
+  monthYearTitle.innerText = `${monthName}, ${year}`;
+
+  // Empty days at start of month
+  for (let i = 0; i < day; i++) {
+    const div = document.createElement("div");
+    div.classList.add("day", "empty");
+    days.appendChild(div);
+  }
+
+  //  Days with moods
+  let numberOfDays = getDaysInMonth(year, month + 1);
+  for (let i = 1; i <= numberOfDays; i++) {
+    const div = document.createElement("div");
+    i === currentDate // Select Currect Date
+      ? div.classList.add("day", "today")
+      : div.classList.add("day");
+    const dayNumber = document.createElement("div");
+    dayNumber.innerText = i;
+    dayNumber.classList.add("day-number");
+    div.appendChild(dayNumber);
+    days.appendChild(div);
+  }
+}
+
+displayCalendar();
+
+// Basic view toggling functionality
+viewButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Remove active class from all buttons and views
+    viewButton.forEach((btn) => btn.classList.remove("active"));
+    views.forEach((view) => {
+      view.classList.remove("active");
+    });
+
+    // Add active class to clicked button
+    button.classList.add("active");
+
+    // Show corresponding view
+    if (button.innerText === "Day") views[0].classList.add("active");
+    if (button.innerText === "Week") views[1].classList.add("active");
+    if (button.innerText === "Month") views[2].classList.add("active");
+  });
+});
